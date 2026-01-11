ec8c28ad7496464fd07c19782ee4e04c1b57843a
Yansh Bhardwaj <yansh.b@momentus.in>
2026-01-11 19:06:38 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Products from './components/Products/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './pages/Root';
 import About from './pages/About';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     children: [
+      {
+        path: '/',
+        element: <Home />
+      },
       {
         path: '/store',
         element: <Products />
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="container mt-5 text-center">
+            <h1>Welcome to the Ecommerce App</h1>
+            <p>Explore our wide range of products and enjoy a seamless shopping experience!</p>
+            <a href="/store" className="btn btn-primary">Shop Now</a>
+        </div>
+    )
+}
+
+export default Home;
